9df2af2db0fe7f36fb63393d26fe0363d87e29af
Chris Paynter <chris@chrispaynter.com>
2020-11-19 20:53:56 +0200

    Moved to use classic rule spec

--- builder.ts
+++ builder.ts
@@ -1,26 +1,37 @@
 import fs from 'fs';
 
-type RuleType = "domains" | "hosts";
+type RemoteType = "domains" | "hosts";
 
-const createTemplate = (type: RuleType, name:string, description:string, domains:string[], source:string) => `{
+const createRule = (type: RemoteType, remote:string, source:string) => 
+`
+    {
+      "action": "deny",
+      "notes": "Sourced from ${source}",
+      "process": "any",
+      "remote-${type}": "${remote}"
+    }`;
+const createTemplate = (type: RemoteType, name:string, description:string, remotes:string[], source:string, numberOfRules: number) => `{
   "name": "${name}",
-  "description": "Last updated ${(new Date()).toUTCString()}. \\n\\n${description}",
-  "denied-remote-${type}": [${domains.map(u => `"${u}"`).join(', ')}],
-  "denied-remote-notes": "Rule from ${source}"
+  "description": "Last updated ${(new Date()).toUTCString()}. \\n\\n${description} \\n\\nContains ${numberOfRules} ${type}",
+  "rules": [
+    ${remotes.map(u => createRule(type, u, source)).join(',')}
+  ]
 }`;
 
 
-export const ruleSetBuilder = (type: RuleType, name:string, description:string, source:string) => {
+export const ruleSetBuilder = (type: RemoteType, name:string, description:string, source:string) => {
 
   let domains:string[] = [];
+  let domainCount:number = 0;
 
   const addDeniedDomain = (domain:string) => {
     domains.push(domain);
+    domainCount++;
   }
 
   const build = (outputDest:string) => {
     const writer = fs.createWriteStream(outputDest);
-    writer.write(createTemplate(type, name, description, domains, source))
+    writer.write(createTemplate(type, name, description, domains, source,domainCount))
     writer.close();
   }
 
