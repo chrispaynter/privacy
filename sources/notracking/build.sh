c0646287176d49f75fdebd2d1871db1ae4522ef1
Chris Paynter <chris@chrispaynter.com>
2020-11-19 18:04:22 +0200

    Added build

---
+++ sources/notracking/build.sh
@@ -0,0 +1,4 @@
+npx ts-node sources/notracking/clean-hostnames.ts
+npx ts-node sources/notracking/build-hostnames.ts
+npx ts-node sources/notracking/clean-domains.ts
+npx ts-node sources/notracking/build-domains.ts
